43c1acd525be27857b9e768da66e142e3b8decd6
Zalesny Sergey <cj1oh@yandex.ru>
2013-03-03 20:17:55 +0400

    Rock lee Chunin exams testing.

--- campaign/power_of_youth/personstat.tcl
+++ campaign/power_of_youth/personstat.tcl
@@ -3,4 +3,4 @@ set tai 4
 set gen 2
 set nin 1
 set speed 3
-set skills {suiken shofu konoha-senpu konoha-dai-senpu konoha-goriki-senpu konoha-congoriki-senpu tsuten-kyaku hachimon-1 hachimon-2 hachimon-3 hachimon-4 hachimon-5 hachimon-6 hachimon-7 hachimon-8 omote-renge ura-renge asakujaku hirudora}
+set skills {suiken shofu konoha-senpu konoha-dai-senpu hachimon-1 hachimon-2 hachimon-3 hachimon-4 hachimon-5 omote-renge ura-renge}
